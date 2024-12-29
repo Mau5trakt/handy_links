ac5fc545b7a72827e50af5844b6a547b789d493c
Mau5trakt <kubzalemany@hotmail.com>
2024-12-28 18:34:09 -0600

    create header

---
+++ links/static/dist/project.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    let toggleAddButton = () => {
+        document.querySelector("#add-btn-dropdown").classList.toggle('dropdown__active');
+        document.querySelector('.header__wrapper--nav--ul-button').classList.toggle('button__active');
+    }
+
+    let mobileMenuToggle = () => document.querySelector("#mobilemenuctn").classList.toggle('mobile-menu__active')
+
+
+    document.addEventListener('click', event =>{
+        if (event.target.closest(".header__wrapper--nav--ul-button")){
+            toggleAddButton()
+        }else if(event.target.closest("#mobilemenu")){
+            mobileMenuToggle()
+        }
+    })
+})
+
+
