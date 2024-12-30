ce82efdced06235d2ee233e31a517eb1e90b331b
Mau5trakt <kubzalemany@hotmail.com>
2024-12-30 01:54:01 -0600

    correct models and add new method

--- links/models.py
+++ links/models.py
@@ -15,7 +15,7 @@ class CustomUser(AbstractUser):
 
 class Folder(models.Model):
     id = models.UUIDField("Id", primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField('Folder Name', max_length=100)
+    title = models.CharField('Folder Name', max_length=100)
     owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="Folder_Owner", verbose_name="Owner")
     public = models.BooleanField('Available to everyone?', default=False)
     collaborators = models.ManyToManyField(CustomUser, related_name='collaborated_folders', blank=True, verbose_name='Collaborators List')
@@ -29,7 +29,7 @@ class Folder(models.Model):
         verbose_name_plural = 'Folders'
 
     def __str__(self):
-        return f'Folder {self.name} by {self.owner}'
+        return f'Folder {self.title} by {self.owner}'
 
     def get_collaborators(self):
         return list(self.collaborators.values_list('username', flat=True))
@@ -39,11 +39,15 @@ class Folder(models.Model):
         """Return the absolute URL for the folder."""
         return reverse('folder_detail', kwargs={'pk': self.pk})
 
+    def get_link_qty(self):
+        return self.Links.count()
+
 
 
 class Link(models.Model):
     folder = models.ForeignKey(Folder, on_delete=models.CASCADE, related_name='Links', verbose_name='Folder')
+    title = models.CharField(max_length=50)
     url = models.TextField('Link Url',max_length=1000)
     owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="Link_Owner", verbose_name="Link Owner")
-    description = models.TextField('Link Description')
+    description = models.TextField('Link Description', blank=True)
 
