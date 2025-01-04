118b315a5db08a1262a85a0b35bbbd41147dd6cc
Mau5trakt <kubzalemany@hotmail.com>
2025-01-03 23:10:24 -0600

    add routes for user autocomplete search and create folder

--- links/urls.py
+++ links/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from links import views
 
 
@@ -7,6 +7,8 @@ urlpatterns = [
     path('', views.RegisterUserView.as_view(), name='register'),
     path('login/', views.MyLoginView.as_view(), name='login'),
     path('home/', views.Home.as_view(), name='home'),
-    path('logout/', views.logoutuser, name='logout')
+    path('logout/', views.logoutuser, name='logout'),
+    re_path(r'^user-autocomplete/$', views.UsersAutocomplete.as_view(), name='user-autocomplete'),
+    path('create-folder/', views.FolderCreationView.as_view(), name='create-folder')
 
 ]
